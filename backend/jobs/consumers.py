5fb5ca3872ec33a7f0dbf407e66ae090f49b4168
Agnes-Kalunda <agypeter97@gmail.com>
2026-09-17 14:11:55 +0300

    refactor

--- backend/jobs/consumers.py
+++ backend/jobs/consumers.py
@@ -1,12 +1,18 @@
 import json
 
+from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.db.models import Q
+
+from jobs.models import JobListing
 
 
 class JobSearchConsumer(AsyncWebsocketConsumer):
     group_name = "search_broadcast"
 
     async def connect(self):
+        self.keywords = ""
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
@@ -15,23 +21,49 @@ class JobSearchConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        keywords = data.get("keywords", "")
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                "type": "search.started",
-                "keywords": keywords,
-            },
-        )
+        keywords = data.get("keywords", "").strip()
+        self.keywords = keywords
 
-    async def search_started(self, event):
         await self.send(text_data=json.dumps({
             "event": "search_started",
-            "keywords": event["keywords"],
+            "keywords": keywords,
         }))
 
+        for job in await self.find_existing_matches(keywords):
+            await self.send(text_data=json.dumps({
+                "event": "job_match",
+                "job": job,
+            }))
+
+        await self.send(text_data=json.dumps({"event": "search_complete"}))
+
+        from jobs.tasks import run_all_scrapers
+        await sync_to_async(run_all_scrapers.delay)()
+
+    @database_sync_to_async
+    def find_existing_matches(self, keywords):
+        query = Q(title__icontains=keywords) | Q(company__icontains=keywords)
+        jobs = JobListing.objects.filter(query).order_by("-scraped_at")[:50]
+        return [
+            {
+                "id": job.id,
+                "title": job.title,
+                "company": job.company,
+                "location": job.location,
+                "url": job.source_url,
+            }
+            for job in jobs
+        ]
+
+    def matches_keywords(self, job):
+        if not self.keywords:
+            return False
+        haystack = f"{job['title']} {job['company']} {job['location']}".lower()
+        return self.keywords.lower() in haystack
+
     async def job_match(self, event):
-        await self.send(text_data=json.dumps({
-            "event": "job_match",
-            "job": event["job"],
-        }))
+        if self.matches_keywords(event["job"]):
+            await self.send(text_data=json.dumps({
+                "event": "job_match",
+                "job": event["job"],
+            }))
